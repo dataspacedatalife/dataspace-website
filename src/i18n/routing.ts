e6f448d2b78cdb47dae764fc68549d58f1985de7
Lara Vázquez <lvazquez@cesga.es>
2025-09-12 13:33:45 +0200

    Add type-safe translation names

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,4 +1,5 @@
 import { defineRouting } from 'next-intl/routing';
+import type messages from '../../messages/en.json';
 
 export const routing = defineRouting({
   // A list of all locales that are supported
@@ -7,3 +8,10 @@ export const routing = defineRouting({
   // Used when no locale matches
   defaultLocale: 'es',
 });
+
+declare module 'next-intl' {
+  interface AppConfig {
+    Locale: (typeof routing.locales)[number];
+    Messages: typeof messages;
+  }
+}
